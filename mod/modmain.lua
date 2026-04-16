9b02a46e41cd0636fb5338b8ec10b75c6d81a78a
Gérison Sabino <gerison@outlook.com.br>
2026-04-16 18:00:11 -0300

    R41_ST_WX78 (release)

--- mod/modmain.lua
+++ mod/modmain.lua
@@ -17,10 +17,7 @@ local SKILLTREE_PREFABS = skilltree_prefabs_table({
 	"winona",
 	"walter",
 	"wendy",
-	"wortox"
-})
-
-local SKILLTREE_PREFABS_BETA = skilltree_prefabs_table({  
+	"wortox",
 	"wx78"
 })
 
@@ -28,8 +25,7 @@ AddPrefabPostInit("world", function(world)
 	world:ListenForEvent("playeractivated", function(world, player)
 		if player == GLOBAL.ThePlayer and 
 		   player.components.skilltreeupdater and
-		   (SKILLTREE_PREFABS[player.prefab] or 
-		   (CurrentRelease.GreaterOrEqualTo("R41_ST_WX78") and SKILLTREE_PREFABS_BETA[player.prefab])) 
+		   SKILLTREE_PREFABS[player.prefab]
 		then
 			GLOBAL.TheGenericKV:SetKV("fuelweaver_killed", "1")
 			GLOBAL.TheGenericKV:SetKV("celestialchampion_killed", "1")
